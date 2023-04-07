ffbbd6924c27e3f7db8bbfc8acdb85805da7e05c
Lotus0316 <105208023+Lotus0316@users.noreply.github.com>
2023-04-07 10:10:31 +0800

    脚本启动版本
    加入app.run()来启动

--- learning_app_flash.py
+++ learning_app_flash.py
@@ -6,7 +6,7 @@ from flask_wtf import FlaskForm # 表单类
 from wtforms import StringField, SubmitField# 添加两个表单属性类
 from wtforms.validators import DataRequired# 添加验证器
 
-app = Flask('FirstWeb')
+app = Flask(__name__)
 
 bootstrap = Bootstrap(app)# 初始化Bootstrap对象
 moment = Moment(app)# 同样初始化Moment
@@ -39,3 +39,6 @@ def name(user):# 这里要调用name模板了
 def page_not_found(e):
     return render_template('404.html'),404
 # 这里参数通常为e（Exception）,其可以访问该异常对象，同时别忘了return404状态码
+
+if __name__=="__main__":
+    app.run()
